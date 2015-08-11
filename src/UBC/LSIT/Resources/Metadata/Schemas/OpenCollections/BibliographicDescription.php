c92fbecdc292fe3e9ea5b22721c99ab44ce9f666
Sean McNamara <sean.mcnamara@ubc.ca>
2015-08-11 13:19:37 -0700

    Publication description properties moved to bibliographic descriptions

--- src/UBC/LSIT/Resources/Metadata/Schemas/OpenCollections/BibliographicDescription.php
+++ src/UBC/LSIT/Resources/Metadata/Schemas/OpenCollections/BibliographicDescription.php
@@ -1,80 +1,29 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: hajime
-     * Date: 10 Aug 15
-     * Time: 23:23
-     */
-    
-    namespace UBC\LSIT\Resources\Metadata\Schemas\OpenCollections;
-
-    use OpenLibrary\Metadata\Schemas\AbstractProperty;
-
-    trait BibliographicDescription {
-
-        /**
-         * @var AbstractProperty
-         */
-        protected $Category;
-
-        /**
-         * @var AbstractProperty
-         */
-        protected $Contents;
-
-        /**
-         * @var AbstractProperty
-         */
-        protected $Edition;
-
-        /**
-         * @var AbstractProperty
-         */
-        protected $PersonOrCorporation;
-
-        /**
-         * @var AbstractProperty
-         */
-        protected $UBCCallNumber;
-
-        /**
-         * @return AbstractProperty
-         */
-        public function getCategory () {
-
-            return $this->Category;
-        }
-
-        /**
-         * @return AbstractProperty
-         */
-        public function getContents () {
-
-            return $this->Contents;
-        }
-
-        /**
-         * @return AbstractProperty
-         */
-        public function getEdition () {
-
-            return $this->Edition;
-        }
-
-        /**
-         * @return AbstractProperty
-         */
-        public function getPersonOrCorporation () {
-
-            return $this->PersonOrCorporation;
-        }
-
-        /**
-         * @return AbstractProperty
-         */
-        public function getUBCCallNumber () {
-
-            return $this->UBCCallNumber;
-        }
-
-    }
+//    /**
+//     * Created by PhpStorm.
+//     * User: hajime
+//     * Date: 10 Aug 15
+//     * Time: 23:23
+//     */
+//
+//    namespace UBC\LSIT\Resources\Metadata\Schemas\OpenCollections;
+//
+//    use OpenLibrary\Metadata\Schemas\AbstractProperty;
+//
+//    trait BibliographicDescription {
+//
+//
+//        /**
+//         * @var AbstractProperty
+//         */
+//        protected $UBCCallNumber;
+//
+//        /**
+//         * @return AbstractProperty
+//         */
+//        public function getUBCCallNumber () {
+//
+//            return $this->UBCCallNumber;
+//        }
+//
+//    }
